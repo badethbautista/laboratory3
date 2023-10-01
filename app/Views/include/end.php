7082b9539b0786a4b2c69073d97762886e8832ee
Bernadeth Bautista <badethbautista07@gmail.com>
2023-10-01 21:30:14 +0800

    neext

---
+++ app/Views/include/end.php
@@ -0,0 +1,4 @@
+<script src="assets/js/plugins.js"></script>
+
+<!-- Main JS -->
+<script src="assets/js/main.js"></script>
--- app/Views/index.php
+++ app/Views/index.php
@@ -1,3 +1,9 @@
 <?= $this->include('include/top') ?>
 
 <?= $this->include('include/header') ?>
+
+<?= $this->include('include/slider') ?>
+
+
+
+<?= $this->include('include/end') ?>
